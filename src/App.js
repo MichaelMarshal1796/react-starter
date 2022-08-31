d9fcc3eb1698a28649c12b3f30403b573c90a626
Michael Marshal <michael.marshal@1billiontech.com>
2022-08-31 16:05:34 +0530

    added build spec

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <input type="password" value="asd" />
         <a
           className="App-link"
           href="https://reactjs.org"
